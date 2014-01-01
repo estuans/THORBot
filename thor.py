53a1bd04313f61023a289cc3908f73146fd91be3
QuasarDelta <darkgreenzenith@gmail.com>
2014-01-01 20:42:43 +0100

    Moved Markov functions to module to clean up thor.py

--- thor.py
+++ thor.py
@@ -9,22 +9,22 @@ WolframAlpha integration will come later.
 from twisted.words.protocols import irc
 
 #INTERNAL Imports
-from modules import wolfram
+from modules.marthor import generate_sentence
+from modules.marthor import add_to_brain
+from modules import observer
 
 #SYS Imports
 import time
 import random
 import re
-import os
-from collections import defaultdict
 
 #OTHER Imports
 import ConfigParser
 import wolframalpha
 
 #Basic Inf
-versionName = "Apple"
-versionNumber = "0.0302a"
+versionName = "Baktu"
+versionNumber = "0.0402a"
 
 #Config parser. Could be replaced in the future?
 
@@ -37,43 +37,11 @@ logfile = cfg.get('Connection', 'Logfile')
 
 #api_id = wolframalpha.Client('YT6T34-E7L5L5YWWK')
 
-#MARKOV integration
-markov = defaultdict(list)
-STOP_WORD = ""
-
-
-def add_to_brain(msg, chain_length, write_to_file=False):
-    if write_to_file:
-        f = open('brain.txt', 'a')
-        f.write(msg + '\r')
-        f.close()
-    buf = [STOP_WORD] * chain_length
-    for word in msg.split():
-        markov[tuple(buf)].append(word)
-        del buf[0]
-        buf.append(word)
-    markov[tuple(buf)].append(STOP_WORD)
-
-
-def generate_sentence(msg, chain_length, max_words):
-    buf = msg.split()[:chain_length]
-    if len(msg.split()) > chain_length:
-        message = buf[:]
-    else:
-        message = []
-        for i in xrange(chain_length):
-            message.append(random.choice(markov[random.choice(markov.keys())]))
-        for i in xrange(max_words):
-            try:
-                next_word = random.choice(markov[tuple(buf)])
-            except IndexError:
-                continue
-            if next_word == STOP_WORD:
-                return
-            message.append(next_word)
-            del buf[0]
-            buf.append(next_word)
-        return ' '.join(message)
+#Globals(These are ABSOLUTELY necessary to declare here, otherwise the code won't work.
+#Don't sue me. I'm just making sure it won't throw syntax warnings at me.)
+icl = irc.IRCClient
+i = irc.IRC
+chttb = True
 
 
 class Bin:
@@ -104,12 +72,16 @@ class ThorBot(irc.IRCClient):
     def __init__(self):
         nickname = cfg.get('Bot Settings', 'Nickname')
         realname = cfg.get('Bot Settings', 'Realname')
+        password = cfg.get('Bot Settings', 'NickPass')
         owner = cfg.get('Users', 'Owner')
         admins = cfg.get('Users', 'Admins')
+        ignored = cfg.get('Users', 'Ignorelist')
         self.nickname = nickname
         self.realname = realname
+        self.password = password
         self.owner = owner
         self.admins = admins
+        self.ignored = ignored
 
     def connectionMade(self):
         #First we connect
@@ -127,6 +99,10 @@ class ThorBot(irc.IRCClient):
         self.logger.log("[%s]Erroneus Nickname" % time.asctime(time.localtime(time.time())))
         if not self._registered:
             self.setNick(self.fbnick)
+
+    def irc_ERR_PASSWDMISMATCH(self, prefix, params):
+        print "!!!INCORRECT PASSWORD!!!\n Check hammer.ini for the NICKPASS parameter"
+        return
 
     #EVENTS
 
@@ -147,10 +123,10 @@ class ThorBot(irc.IRCClient):
         chance = random.random()
 
         if chance >= 0.2:
-                print "Welcomed %s. Chance: %s" % (user, chance)
-                msg = "Welcome to %s, %s" % (channel, user)
-                self.logger.log(msg)
-                self.msg(channel, msg)
+            print "Welcomed %s. Chance: %s" % (user, chance)
+            msg = "Welcome to %s, %s" % (channel, user)
+            self.logger.log(msg)
+            self.msg(channel, msg)
         if chance <= 0.2:
             print "Chance:", chance
         self.logger.log("%s has joined %s" % (user, channel))
@@ -166,12 +142,16 @@ class ThorBot(irc.IRCClient):
         msg = "{us} must have pissed {ki} off!".format(us=kickee, ki=kicker)
         self.msg(channel, msg)
 
+    def irc_unknown(self, prefix, command, params):
+        if command == "INVITE":
+            self.join(params[1])
+
     def privmsg(self, user, channel, msg):
         user = user.split('!', 1)[0]
         self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=msg))
 
-        #Used by the Join command
-        nch = []
+        #Globals
+        global chttb
 
         #Used by the Markov command
         prefix = "%s: " % user
@@ -181,27 +161,78 @@ class ThorBot(irc.IRCClient):
         max_words = cfg.getint('Bot Settings', 'Max Words')
         owner = cfg.get('Users', 'Owner')
         admins = cfg.get('Users', 'Admins')
+        ignored = cfg.get('Users', 'Ignorelist')
 
        #Thor's markov implementation wasn't playing ball, so I had
        #To alter the vast majority of the code. Instead of replying randomly,
        #He now only generates when prompted by the !markov command.
        #It's not the best way, but it's the least intrusive.
 
+        if msg:
+            #Logs all messages
+            self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=msg))
+
         if self.msg:
             #Logs own messages, so long as self.msg() is called
             self.logger.log("[{c}] {u}: {m}".format(c=channel, u=self.nickname, m=self.msg))
 
-        if msg.__contains__(self.nickname):
+        if msg == "!reload":
+            msg = "Reloading..."
+            self.msg(channel, msg)
+            observer.remodule()
+
+        if msg == "!chatterbot":
+            #Checks what the status of the chttb variable is and provides it in place of {st}
+            msg = "Chatterbot variable status: {st}".format(st=chttb)
+            self.msg(channel, msg)
+
+        if msg == "!chatterbot off":
+            #Checks if chttb is true, and if it is, changes it to False.
+            if chttb is True:
+                chttb = False
+                msg = "Chatterbot replies turned off."
+                self.msg(channel, msg)
+            else:
+                msg = "Chatterbot replies already off."
+                self.msg(channel, msg)
+
+        if msg == "!chatterbot on":
+            #Inverse of the above
+            if chttb is False:
+                chttb = True
+                msg = "Chatterbot replies turned on."
+                self.msg(channel, msg)
+            else:
+                msg = "Chatterbot replies already on."
+                self.channel(channel, msg)
+
+        if msg.__contains__(self.nickname) and chttb is True and user != ignored:
             #Silly command that just produces a bunch of garbled text from brain.txt
             #Might be expanded upon at a later time, but in all likelihood
             #I'll try to replace it with a natural learning/machine learning command.
-            msg = re.compile(self.nickname + "[:,]* ?").sub('', msg)
+            #msg = re.compile(self.nickname + "[:,]* ?").sub('', msg)
+            #msg = re.compile(self.nickname + "[a-z$1][A-Z$1]+[\s^$]$1").sub("", msg)
+            msg = re.compile(self.nickname + "[A-Z][a-z'\-]+[, ]([a-zA-Z'\-]+[;,]? ){15,25}[a-zA-Z'\-]+[.?!]").sub("",
+                                                                                                                   msg)
             sentence = generate_sentence(msg, chain_length, max_words)
             if sentence:
-                ' '.join(sentence.split())
+                #' '.join(sentence.split("[:,]* ?"))
+                ' '.join(sentence.split("[A-Z][a-z'\-]+[, ]([a-zA-Z'\-]+[;,]? ){15,25}[a-zA-Z'\-]+[.?!]"))
                 re.split(msg, sentence)
-                self.msg(channel, prefix + sentence + ".")
-                self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=sentence))
+                if sentence.endswith(""):
+                    self.msg(channel, prefix + sentence + ".")
+                    print "!!!DEBUG: Replaced WHITESPACE!!!"
+                elif sentence.endswith(","):
+                    sentence.replace(",", ".")
+                    self.msg(channel, prefix + sentence)
+                    print "!!!DEBUG: Replaced COMMA!!!"
+                elif sentence.endswith(",."):
+                    sentence.replace(",.", ".")
+                    self.msg(channel, prefix + sentence)
+                    print "!!!DEBUG: Replaced COMMA PERIOD!!!"
+                else:
+                    self.msg(channel, prefix + sentence)
+                self.logger.log("[{c}] {u}: {m}".format(c=channel, u=self.nickname, m=self.msg))
                 print sentence
             else:
                 print "Markov function called, but failed to generate sentence."
@@ -214,9 +245,11 @@ class ThorBot(irc.IRCClient):
             msg = "Version: {vnam}(NUMBER {vnum})".format(vnam=versionName, vnum=versionNumber)
             self.msg(channel, msg)
 
-        if msg == "!inv {user} {chan}".format(user=user, chan=channel):
+        if msg == "!inv {user} {chan}":
             #Broken. Meant to invite person.
-            self.invite(user, channel)
+            user = user
+            chan = channel
+            self.invite(user, chan)
             msg = "Inviting {user} into {chan}".format(user=user, chan=channel)
             self.msg(channel, msg)
             self.logger.log("Invited {user} into {chan}".format(user=user, chan=channel))
@@ -227,38 +260,33 @@ class ThorBot(irc.IRCClient):
                   "Never gonna run around and desert you.\n" \
                   "Never gonna make you cry,\nNever gonna say goodbye,\nNever gonna tell a lie and hurt you.\n"
             self.msg(channel, msg)
-            self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=self.msg))
 
         if msg.startswith("!info"):
             #If called, states owner and nickname
             owner = cfg.get('Users', 'Owner')
             msg = "Hello, {u}. I am {n}, a bot belonging to {o}".format(u=user, n=self.nickname, o=owner)
-            self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=self.msg))
+            self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=msg))
             self.msg(channel, msg)
 
-        if msg.startswith("!leave %s" % channel) and user == (owner or admins):
+        if msg.startswith("!leave {c} {r}".format(c=channel, r=str)) and user == (owner or admins):
             #Leaves the channel(can be called outside said channel?)
             self.logger.log("[{c}] {u}: {m}".format(c=channel, u=user, m=msg))
-            self.leave(channel)
+            self.leave(channel, reason=reason)
 
         if msg == "!disconnect" and user == (owner or admins):
             self.quit(message="Disconnected per request")
             self.logger.log("Disconnected per request of %s" % user)
 
-        if msg == "!join {ch}".format(ch=nch):
-            #Joins designated channel.
-            nch = channel
-            self.join(nch)
-
         if channel == self.nickname:
-            msg = "I don't reply to whispers."
-            self.logger.log("<%s> %s" % (self.nickname, self.msg))
-            self.msg(user, msg)
+            if user != owner:
+                msg = "I don't reply to whispers."
+                self.logger.log("<{t}> {m}".format(t=self.nickname, m=self.msg))
+                self.msg(user, msg)
+            elif user == owner:
+                if msg == "!s {ch} ".format(ch=channel):
+                    self.say(channel, msg)
+                    self.logger.log("[{c}] <{t}> {m}".format(c=channel, t=self.nickname, m=self.msg))
             return
-
-        if msg == self.nickname + "how many channels are there?":
-            msg = "There are {nchan} channel(s) on this network.".format(nchan=self.luserChannels(channels=int))
-            self.msg(channel, msg)
 
     def action(self, user, channel, message):
         user = user.split('!', 1)[0]
@@ -269,7 +297,7 @@ class ThorBot(irc.IRCClient):
     def irc_NICK(self, prefix, params):
         old_nick = prefix.split('!')[0]
         new_nick = params[0]
-        self.logger.log("%s has changed nick to %s" % (old_nick, new_nick))
+        self.logger.log("{on} has changed nick to {nn}".format(on=old_nick, nn=new_nick))
 
     def alterCollidedNick(self, nickname):
         return nickname + 'Clone'
