536bd75c681de4731df2c13a4ed6edc85a73da99
QuasarDelta <darkgreenzenith@gmail.com>
2014-11-19 19:29:46 +0100

    Fixed KeyError from msg-reminder

--- src/thor.py
+++ src/thor.py
@@ -114,15 +114,22 @@ class ThorBot(irc.IRCClient):
     def privmsg(self, user, channel, msg):
         user = user.split('!', 1)[0]
 
-        if not msg.startswith('!remind'):
+        if msg:
             sh = shelve.open('reminders')
             rfor = user
-            reminder = sh[rfor]
-            reply = "[%s] %s" % (user, reminder)
-            self.msg(channel, reply)
+            check = sh.has_key(rfor)
 
-            #And deletes them
-            del sh[rfor]
+            if check is True:
+                #Checks if key exists
+                reminder = sh[rfor]
+                reply = "[%s] %s" % (user, reminder)
+                self.msg(channel, reply)
+
+                #And deletes them
+                del sh[rfor]
+
+            elif check is False:
+                pass
 
         #Calculator
 
