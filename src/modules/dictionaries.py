f3b630e5bd1f284070829226f85d560d8801de9d
QuasarDelta <darkgreenzenith@gmail.com>
2014-11-20 17:08:52 +0100

    Dictionary module keeps the main file clean

---
+++ src/modules/dictionaries.py
@@ -0,0 +1,30 @@
+"""
+Words are not phrases, merely the building blocks of hazes.
+
+Contains various dictionaries for use in thor.py.
+Keeping this file around for ease of use and orderliness.
+"""
+
+
+class Randict():
+
+    shakespeare = ["A fool thinks himself to be wise, but a wise man knows himself to be a fool", "If music be the food"
+                                                                                                  " of love, play on.",
+                   "It is not the stars that hold our destiny, but in ourselves.", "Love all, trust few, do wrong to"
+                                                                                   " none",
+                   "We know what we are, but know not what we may be.", "Good night, good night! Parting is such a"
+                                                                        " sweet sorrow, that I shall say good night"
+                                                                        " till it be 'morrow!",
+                   "There is nothing either good or bad, but thinking makes it so.", "No legacy is so rich as honesty.",
+                   "Some are born great, some achieve greatness, and some have greatness thrust upon them.",
+                   "One touch of nature makes the whole world kin.", "Hell is empty, and all the devils are here.",
+                   "Listen to many, speak to a few.", "The course of true love never did run smooth.", "This above all;"
+                                                                                                       " to thine own"
+                                                                                                       " self be true",
+                   "Listen to many, speak to a few.", "The wheel is come full circle.", "Love is a smoke made with"
+                                                                                        " the fume of sighs.",
+                   "What's in a name? That which we call a rose by any other name would smell as sweet.", "To be, or"
+                                                                                                          " not to be, "
+                                                                                                          "that is the "
+                                                                                                          "question.",
+                   "They do not love, that do not show their love.", "Suspicion always haunts the guilty mind.",]
